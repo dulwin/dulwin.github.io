f8eb4ce8978b4638cbfc99e4a4ec8f557ef3c67d
Dulwin Jayalath <dulwinjayalath@Dulwins-MacBook-Pro.local>
2015-01-12 00:48:32 -0500

    Header started

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
-<?php include "header.php"; ?>
+<html>
+    <head>
+        <title>Dulwin Jayalath</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="style/style.css">
+    </head>
 
-<?php include "footer.php"; ?>
+    <body>
+        <nav class="navbar navbar-default navbar-fixed-top" id="header">
+            <div class="container">
+
+            </div>
+        </nav>
+    </body>
+
+    <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+</html>
