b0401b2911c250b7b9eb3b7604a4417fa4e8b1fc
Dulwin Jayalath <dulwinjayalath@Dulwins-MacBook-Pro.local>
2015-01-12 00:40:00 -0500

    Header started

---
+++ header.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Dulwin Jayalath</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="style/style.css">
+    </head>
+
+    <body>
+        <nav class="navbar navbar-default navbar-fixed-top" id="header">
+            <div class="container">
+
+            </div>
+        </nav>
